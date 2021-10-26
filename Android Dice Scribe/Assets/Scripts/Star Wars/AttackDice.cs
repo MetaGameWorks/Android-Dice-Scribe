1624635be3b742d31bba8190e8a50b2bb6c1a4a5
MGW <45159186+MetaGameWorks@users.noreply.github.com>
2021-10-25 22:04:17 -0400

    Star Wars

--- Android Dice Scribe/Assets/Scripts/Star Wars/AttackDice.cs
+++ Android Dice Scribe/Assets/Scripts/Star Wars/AttackDice.cs
@@ -14,6 +14,12 @@ public class AttackDice : MonoBehaviour
     public GameObject whiteAttackInput;
     public GameObject blackAttackInput;
     public GameObject redAttackInput;
+    // variables for the attack result menu
+    public GameObject numOfCritsOutput;
+    public GameObject numOfHitsOutput;
+    public GameObject numOfSurgesOutput;
+    public GameObject numOfBlanksOutput;
+
     // variables to hold the input values
     private int numOfWhiteAttackDice;
     private int numOfBlackAttackDice;
@@ -72,7 +78,9 @@ public class AttackDice : MonoBehaviour
         Array.Clear(reRollRedAttackDiePool, 0, 8);
 
         // assign values from the input fields
-
+        //int.TryParse(whiteAttackInput.GetComponent<Text>().text, out numOfWhiteAttackDice);
+        //int.TryParse(blackAttackInput.GetComponent<Text>().text, out numOfBlackAttackDice);
+        //int.TryParse(redAttackInput.GetComponent<Text>().text, out numOfRedAttackDice);
 
         // don't roll dice if there were none selected
         if (numOfWhiteAttackDice > 0 || numOfBlackAttackDice > 0 || numOfRedAttackDice > 0)
@@ -86,7 +94,7 @@ public class AttackDice : MonoBehaviour
                 numOfWhiteCrits = whiteAttackDiePool[7];
                 numOfWhiteHits = whiteAttackDiePool[6];
                 numOfWhiteSurges = whiteAttackDiePool[5];
-                numOfWhiteBlanks = whiteAttackDiePool.Sum() - (numOfWhiteCrits + numOfWhiteHits + numOfWhiteSurges);
+                numOfWhiteBlanks = whiteAttackDiePool[4] + whiteAttackDiePool[3] + whiteAttackDiePool[2] + whiteAttackDiePool[1] + whiteAttackDiePool[0];
 
                 // total results
                 totalNumOfCrits += numOfWhiteCrits;
@@ -98,20 +106,79 @@ public class AttackDice : MonoBehaviour
             if(numOfBlackAttackDice > 0)
             {
                 RollDice(blackAttackDiePool, numOfBlackAttackDice);
+
+                // tally black dice
+                numOfBlackCrits = blackAttackDiePool[7];
+                numOfBlackHits = blackAttackDiePool[6] + blackAttackDiePool[5] + blackAttackDiePool[4];
+                numOfBlackSurges = blackAttackDiePool[3];
+                numOfBlackBlanks = blackAttackDiePool[2] + blackAttackDiePool[1] + blackAttackDiePool[0];
+
+                // total results
+                totalNumOfCrits += numOfBlackCrits;
+                totalNumOfHits += numOfBlackHits;
+                totalNumOfSurges += numOfBlackSurges;
+                totalNumOfBlanks += numOfBlackBlanks;
             }
 
             if(numOfRedAttackDice > 0)
             {
                 RollDice(redAttackDiePool, numOfRedAttackDice);
+
+                // tally red dice
+                numOfRedCrits = redAttackDiePool[7];
+                numOfRedHits = redAttackDiePool[6] + redAttackDiePool[5] + redAttackDiePool[4] + redAttackDiePool[3] + redAttackDiePool[2];
+                numOfRedSurges = redAttackDiePool[1];
+                numOfRedBlanks = redAttackDiePool[0];
+
+                // total results
+                totalNumOfCrits += numOfRedCrits;
+                totalNumOfHits += numOfRedHits;
+                totalNumOfSurges += numOfRedSurges;
+                totalNumOfBlanks += numOfRedBlanks;
             }
+
+            // print results
+            //numOfCritsOutput.GetComponent<Text>().text = "" + totalNumOfCrits;
+            //numOfHitsOutput.GetComponent<Text>().text = "" + totalNumOfHits;
+            //numOfSurgesOutput.GetComponent<Text>().text = "" + totalNumOfSurges;
+            //numOfBlanksOutput.GetComponent<Text>().text = "" + totalNumOfBlanks;
+
+            // change menus
+            attackResultMenu.SetActive(true);
+            attackMenu.SetActive(false);
         }
     }
 
+    // rolls to the dicePool array a number of times equal to num
     public void RollDice(int[] dicePool, int num)
     {
         for(int i = 0; i > num; i++)
         {
             dicePool[UnityEngine.Random.Range(0, 8)]++;
         }
+    }
+
+    public void ReRollHit()
+    {
+        // re-roll a hit dice, presumably to get a crit
+
+    }
+
+    public void ReRollSurge()
+    {
+        // re-roll a surge dice
+
+    }
+
+    public void ReRollBlank()
+    {
+        // re-roll a blank dice
+
+    }
+
+    public void PrintReRoll()
+    {
+        // print re-rolls
+
     }
 }
