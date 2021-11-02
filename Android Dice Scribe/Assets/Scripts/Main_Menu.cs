6e4e83e04a008f5ce746f8e723361a9b4f9e217a
MGW <45159186+MetaGameWorks@users.noreply.github.com>
2021-11-01 22:30:50 -0400

    Main Menu Resize
    resized main menu to fit the galaxy tab 7

--- Android Dice Scribe/Assets/Scripts/Main_Menu.cs
+++ Android Dice Scribe/Assets/Scripts/Main_Menu.cs
@@ -5,9 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class Main_Menu : MonoBehaviour
 {
-    public void _40k_stand_dice()
+    public void _40k_default_dice()
     {
         SceneManager.LoadScene("_40k_Stand_Dice");
+    }
+
+    public void SWL_default_dice()
+    {
+        SceneManager.LoadScene("StarWarsLegion");
     }
 
     public void QuitGame()
